dc0427c680bd3c134a9dfc723836e6d1be911178
Naina Jain <naina@Naina-MacBook-Air2.local>
2022-06-14 22:24:15 -0400

    Wrote out code plan

---
+++ Assignment2.R
@@ -0,0 +1,13 @@
+# MSC2011H Special Topics Assignment 2 - Naina Jain - Due June 14, 2022
+# Narcissistic Numbers
+
+# Get user input 
+
+# Check if user input is numeric
+
+# Check if user input is valid
+  # invalid if not three digits
+
+# Check if number is narcissistic, which is a number that is equal to the sum of the cube of its own digits
+
+# Display result with corresponding message
