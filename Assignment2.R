1c657d7648b96329704412d919fc446fcbc2a6b2
Naina Jain <naina@Naina-MacBook-Air2.local>
2022-06-14 23:12:59 -0400

    Input code to test for narcissistic numbers, still getting wrong output message at the end.

--- Assignment2.R
+++ Assignment2.R
@@ -2,12 +2,28 @@
 # Narcissistic Numbers
 
 # Get user input 
+input <- readline(prompt = "Please enter a three digit positive number: ")
 
 # Check if user input is numeric
+input <- as.numeric(input)
 
-# Check if user input is valid
-  # invalid if not three digits
-
-# Check if number is narcissistic, which is a number that is equal to the sum of the cube of its own digits
-
-# Display result with corresponding message
+# Check if user input is narcissistic which is a number that is equal to the sum of the cube of its own digits
+if (is.na(input) || input < 100 || input > 999) {
+  print("Input is invalid. Program shut down.")
+} else {
+  # separate digits, cube, then sum
+  c <- input %% 10 # gives remainder or right most digit
+  b1 <- input %/% 10 # gives double digit integer (ab), still need to separate these digits
+  b <- b1 %% 10 # gives middle digit
+  a <- b %/% 10 # gives left most digit
+  
+  sum.cube <- sum(a^3,b^3,c^3)
+  # check if input is equal to sum.cube
+  if (input == sum.cube) {
+    print("This is a narcissistic number!")
+  } else {
+    print("This is not a narcissistic number! :(")
+    
+  }
+  
+}
